81ea4c63dc88c4a43eb7421de5b616a3d9e41260
Alastair Lyall <2567444l@student.gla.ac.uk>
2022-10-14 22:18:19 +0100

    added api token

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import replicate
-
-model = replicate.models.get("stability-ai/stable-diffusion")
+rep = replicate.Client(api_token="3aad027667c111e232e6bc91732851bedede6b70")
+model = rep.models.get("stability-ai/stable-diffusion")
 output = model.predict(prompt="little goblin man")
 print(output)
 
