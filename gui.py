eba8a9f58481f2f7c5ffc28db30760e0c2c038b6
imcwhinnie <im329mail@gmail.com>
2022-10-15 13:03:51 +0100

    Created hacker typer :)

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from PIL import ImageTk, Image
+import time
 #from ai import generate_image
 #from django.utils.text import slugify
 
@@ -18,9 +19,19 @@ class GUI(tk.Frame):
         self.label = tk.Label(image=self.image)
         """
 
+        # Canvas Variables
+        self.canvaswidth = 1000
+        self.canvasheight = 428
 
-        self.canvas = tk.Canvas(master, width=1000, height=428, bg="blue", relief=tk.FLAT, bd=0)
-        self.canvas.create_text(2, 2, text="HELLO WORLD", fill="black", font=('Helvetica 15 bold'), anchor=tk.NW)
+        self.canvas = tk.Canvas(master, width=self.canvaswidth, height=self.canvasheight, bg="black", relief=tk.FLAT, bd=0)
+       
+        # Text Mode Variables
+        self.cursorpos = (0,0)
+        self.screen_width = 80
+        self.screen_height = 25
+        self.lineheight = self.canvasheight//self.screen_height
+        self.characterwidth = self.canvaswidth//self.screen_width
+        self.font = ('Courier','15','bold')
 
 
         #self.entry.pack()
@@ -28,8 +39,29 @@ class GUI(tk.Frame):
         self.canvas.pack(pady=100)
         #self.submit_button.pack()
 
-    def write_text(self):
-        pass
+        self.write_text('Hello world!\nThe end is nigh.')
+
+    def get_cursor_canvaspos(self):
+        return (self.cursorpos[0]*self.characterwidth,self.cursorpos[1]*self.lineheight)
+
+    def draw_character(self, char):
+        if char == '\n':
+            self.cursorpos = (0, self.cursorpos[1]+1)
+        else:
+            self.canvas.create_text(*self.get_cursor_canvaspos(), text=char, fill='white', font=self.font, anchor=tk.NW)
+            self.cursorpos = (self.cursorpos[0]+1, self.cursorpos[1])
+
+    def key_pressed(self, event):
+        #print('You pressed a key', event.keycode)
+        if event.keycode == 13:
+            self.draw_character('\n')
+        else:
+            self.draw_character(event.char)
+
+    def write_text(self,text):
+        for char in text:
+            self.draw_character(char)
+            #time.sleep(0.05)
 
     def set_image(self):
         prompt = self.prompt_var.get()
@@ -37,8 +69,11 @@ class GUI(tk.Frame):
         #self.image = ImageTk.PhotoImage(file=f"{slugify(prompt)}.png")
         self.label.configure(image=self.image)
 
+def test(event):
+    print('test')
 
 root = tk.Tk()
 root.config(bg='black')
 app = GUI(master=root)
+root.bind('<Key>',app.key_pressed)
 app.mainloop()
