35e622c0c62f2c54803df4212faff57a6fd2ffef
Emma <2577478G@student.ac.uk>
2022-10-15 17:58:48 +0100

    Testing with alex

--- IA tex.py
+++ IA tex.py
@@ -4,3 +4,12 @@ from transformers import pipeline, set_seed
 generator = pipeline('text-generation', model='gpt2')
 set_seed(42)
 print(generator("Hello Emma, I'm your text generator,", max_length=30, num_return_sequences=5))
+
+
+#CASES
+
+#Scene 1: Phone call
+CharName = "Emma" #temp
+x = CharName
+caller = generator("Hello" x ", this is Franchesco, I am calling you to tell you about your inheritence in the south of france, ", max_length=30, num_return_sequences=5)
+print(caller)
