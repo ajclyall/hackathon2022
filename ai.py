4f8a6821be3946a2b9e421e3ba342698e1812e0b
Alastair Lyall <2567444l@student.gla.ac.uk>
2022-10-15 05:16:16 +0100

    i made the gui have a text input to change the image. it is so epic (not. pranked.)

--- ai.py
+++ ai.py
@@ -1,24 +1,21 @@
-
-from PIL import ImageTk, Image
-
 import replicate
 import requests
-
-import gui
+from django.utils.text import slugify
 
 rep = replicate.Client(api_token="3aad027667c111e232e6bc91732851bedede6b70")
 model = rep.models.get("stability-ai/stable-diffusion")
 
-def generate_image(prompt):
-    return model.predict(prompt=prompt)
 
-def display_image():
-    # this whole file is unfinished, please be nice :)
-    data = requests.get(generate_image()[0])
+def generate_image(prompt):
+
+    data = requests.get(model.predict(prompt=prompt)[0]).content
+    with open(f"{slugify(prompt)}.png", "wb") as file:
+        file.write(data)
+
+# def display_image():
+#     # this whole file is unfinished, please be nice :)
+#     data = requests.get(generate_image()[0])
 
 
 # with open("test.png", 'wb') as file:
 #     file.write(data.content)
-
-
-
--- gui.py
+++ gui.py
@@ -1,16 +1,29 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+from PIL import ImageTk, Image
+from ai import generate_image
+from django.utils.text import slugify
 
 
-class GUI:
-    def __init__(self, root=None):
-        self.text_frame = Frame(root)
-        self.image_frame = Frame(root)
+class GUI(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.prompt_var = tk.StringVar()
+        self.entry = tk.Entry(master, textvariable=self.prompt_var)
+        self.submit_button = tk.Button(root, text="submit", command=self.set_image)
+        self.image = tk.PhotoImage(file='test.png')
+        self.label = tk.Label(image=self.image)
+        self.entry.pack()
+        self.label.pack()
+        self.submit_button.pack()
+
+    def set_image(self):
+        prompt = self.prompt_var.get()
+        generate_image(prompt)
+        self.image = ImageTk.PhotoImage(file=f"{slugify(prompt)}.png")
+        self.label.configure(image=self.image)
 
 
-# root = Tk()
-# frm = ttk.Frame(root, padding=10)
-# frm.grid()
-# ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-# root.mainloop()
+root = tk.Tk()
+app = GUI(master=root)
+app.mainloop()
