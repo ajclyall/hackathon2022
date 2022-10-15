ff21a74b4be5410f342cc760fc565f6f6c87b372
Emma <2577478G@student.ac.uk>
2022-10-15 20:52:31 +0100

    Conv Starters

--- Testing with alex.py
+++ Testing with alex.py
@@ -1,23 +1,40 @@
 
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from ConvStarters import caller,townMan
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-#gpt2 = GPT2LMHeadModel.from_pretrained('gpt2')
-
-
-#tokenizer.padding_side = "left"
-#tokenizer.pad_token = tokenizer.eos_token
 model = GPT2LMHeadModel.from_pretrained('gpt2')
 
+
+#THINGS THAT WILL CHANGE IN EACH CASE
+
+#input text
+#length of answer
+max_length = 150
+
+
+
+#Towns man 1
 sequence = ("Hi my name is James \n"
             "I know you inherited the castle \n"
             "Have you heard the infinite horror stories about the bloody viscount \n"
             "He used to live in the castle on the hill, \n"
-            "and he massacred hundreds of towns people \n"
+            "and he massacred hundreds of towns people. \n"
             )
+
+#Baker (town)
+
+func = caller()
+
+sequence = func[0]
+max_length = func[1]
+
+
+
+
 inputs = tokenizer.encode(sequence, return_tensors='pt')
-outputs = model.generate(inputs, max_length=150, do_sample=True, temperature=0.5, top_k=50, no_repeat_ngram_size=2,)
-text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+outputs = model.generate(inputs, max_length, do_sample=True, temperature=0.3,top_p=0.9, top_k=50, no_repeat_ngram_size=2)
+text = tokenizer.decode(outputs[0], skip_special_tokens=True,)
 print(len(text))
 
 
