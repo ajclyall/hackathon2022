a5b8165637b701d202d9b0f42a57bb2e6abf255a
Alastair Lyall <2567444l@student.gla.ac.uk>
2022-10-15 21:44:34 +0100

    do not pull this! this is for testing

--- ai.py
+++ ai.py
@@ -2,14 +2,14 @@ import replicate
 import requests
 from django.utils.text import slugify
 
-rep = replicate.Client(api_token="3aad027667c111e232e6bc91732851bedede6b70")
+rep = replicate.Client(api_token="e53c3e92e92cedf6f1e24a377e261efa92d83a0d")
 model = rep.models.get("stability-ai/stable-diffusion")
 
 
 def generate_image(prompt):
 
     data = requests.get(model.predict(prompt=prompt)[0]).content
-    with open(f"{slugify(prompt)}.png", "wb") as file:
+    with open("image.ppm", "wb") as file:
         file.write(data)
 
 # def display_image():
